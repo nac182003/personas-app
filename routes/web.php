772371a02e3c6977db5bb59e6544353dc0b2755e
nac182003 <nac.182003@gmail.com>
2025-03-26 22:46:18 -0500

    Comuna List

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ComunaController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/comunas', [ComunaController::class, 'index']);
+
+Route::get('/comunas/create', function () {
+    return "Página de creación de comunas en construcción.";
+})->name('comunas.create');
